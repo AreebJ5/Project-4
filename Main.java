63787a5ff0c40afc151dd0ce38b2a9d5ca23cf41
AreebJ5 <areeb.jackson@patelhs.org>
2021-01-29 14:16:21 +0000

    Started working on my Method

--- Main.java
+++ Main.java
@@ -4,59 +4,14 @@ class Main {
   public static void main(String[] args)
   {
     Scanner scan = new Scanner(System.in);
-
-    System.out.println("What is your name?");
-    String name = scan.next();
-    System.out.println("What is your current weight?");
-    int currentYear = scan.nextInt();
-    System.out.println("How much weight did you gain/lose last year?");
-    int birthYear = scan.nextInt();
-
-    int age = getAge(birthYear, currentYear);
-
-    System.out.println("Hello " + (name));
-    System.out.println("Your weight is " + age);
-
-//Scanner for height 
     
-    System.out.println("How tall are you this year in inches? Please give a whole number.");
-    int currentHeight = scan.nextInt();
-    System.out.println("How tall were you in inches last year? Give a whole number.");
-    int lastHeight = scan.nextInt();
+    System.out.println("What is your name?");
 
-    int height = heightGrown(lastHeight, currentHeight);
+    String name = scan.next();
 
-    System.out.println(welcome(name));
-    System.out.println("you grew " + height + " inches last year.");
-  }
+    System.out.println("Hello " + name + " welcome to your doctors appointment");
+    System.out.println("What is your current weight?");
 
-  //method calculates Weight 
-  static int getAge(int birthYear, int currentYear)
-  {
-    int age = currentYear + birthYear;
-    return age;
-  }  
+    int currentWeight = scan.nextInt();
 
-  //method returns greeting
-  
-
- //Create a methoid that takes in at least 1 parameter and returns some type 
- //call this new method within your main method
- 
-   
-  //method calculates age
-  static int heightGrown(int lastHeight, int currentHeight)
-  {
-    int height = currentHeight - lastHeight;
-    return height;
-  }  
-
-  //method returns greeting
-  static String welcome(String name) 
-  {
-    String hello = "Hello. Welcome back, " + name;
-    return hello;
-  }
- 
- 
-}
+    
