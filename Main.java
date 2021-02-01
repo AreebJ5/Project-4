58da61c3ecb728dec76e07a158c673bfcd1f40b6
AreebJ5 <areeb.jackson@patelhs.org>
2021-02-01 18:58:55 +0000

    This is my first Method that finds the patient's weight.

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args)
-  {
+  public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
     
     System.out.println("What is your name?");
@@ -13,5 +12,38 @@ class Main {
     System.out.println("What is your current weight?");
 
     int currentWeight = scan.nextInt();
+   System.out.println("How much weight did you gain/lose last year?");
+    System.out.println("If you gained weight use a positive number. If you lost weight use a negative number.");
 
+    int weightLostGained = scan.nextInt();
+
+    int weight = getWeight(weightLostGained , currentWeight);
+
+    System.out.println("Hi " + (name));
+    System.out.println("Your weight is " + weight);
+
+    System.out.println(welcome(name));
+    }
+
+    //method calculates Weight 
+    static int getWeight(int weightLostGained , int currentWeight)
+   {
+    int weight = currentWeight + weightLostGained ;
+    return weight;
+   }  
+
+ 
+ 
+   
+
+
+   //method returns greeting
+   static String welcome(String name) 
+   {
+    String hello = "Hello. Welcome back, " + name;
+    return hello;
+   }
+ 
+ 
+}
     
